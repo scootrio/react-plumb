71de9b589f1ee7ded3bff0e0c57ee036dbda0832
Braden Hitchcock <developer.hitchcock@gmail.com>
2019-12-09 12:44:35 -0800

    feat: add support for default endpoint options

--- hoc.js
+++ hoc.js
@@ -6,25 +6,26 @@ import { EndpointOptions } from 'jsplumb';
  * available to the `plumb()` function returned from the `usePlumbContainer()` hook.
  *
  * @param {EndpointOptions[]} endpoints
+ * @param {EndpointOptions} options
  * @returns {React.Component} a new component with the endpoint options as a default property
  */
-export function withEndpoints(endpoints) {
-    return function(Component) {
-      function ComponentWithEndpoints(props) {
-        return createElement(Component, props);
-      }
-  
-      // We apply the endpoints to the default props so that
-      //
-      // 1) the developer doesn't need to explicitly include the prop when using the component, and
-      // 2) while doing so, the endpoints will be visible to the `usePlumbContainer()` hook
-      //
-      // This comes with the added advantage that, if the developer needs to override endpoint settings for certain
-      // instances, they can by passing in an `endpoints` property to the component.
-      ComponentWithEndpoints.defaultProps = {
-        endpoints
-      };
-  
-      return ComponentWithEndpoints;
+export function withEndpoints(endpoints, options = {}) {
+  return function(Component) {
+    function ComponentWithEndpoints(props) {
+      return createElement(Component, props);
+    }
+
+    // We apply the endpoints to the default props so that
+    //
+    // 1) the developer doesn't need to explicitly include the prop when using the component, and
+    // 2) while doing so, the endpoints will be visible to the `usePlumbContainer()` hook
+    //
+    // This comes with the added advantage that, if the developer needs to override endpoint settings for certain
+    // instances, they can by passing in an `endpoints` property to the component.
+    ComponentWithEndpoints.defaultProps = {
+      endpoints: endpoints.map(e => ({ ...options, ...e }))
     };
-  }
+
+    return ComponentWithEndpoints;
+  };
+}
